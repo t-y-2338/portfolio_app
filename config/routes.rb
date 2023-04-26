d994f8d776ac3d7a17e4cd51c5133ae927aca54f
t-y-2338 <yoshi_rr@MacBook-Air1.local>
2023-04-26 14:19:14 +0900

    feat: #7 sessionsコントローラーを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
