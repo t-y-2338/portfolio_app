1b8938daf62415b2de69c2ea0b197d41c1d5005c
t-y-2338 <yoshi_rr@MacBook-Air1.local>
2023-04-26 23:57:40 +0900

    feat: #7 ログイン画面を仮作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
 
   resources :users, only: %i[new create]
 end
