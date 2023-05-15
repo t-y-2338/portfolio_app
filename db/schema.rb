74ee9933e7d920dd0ac5fe0183164e1bd480648e
t-y-2338 <yoshi_rr@MacBook-Air1.local>
2023-05-16 01:29:28 +0900

    refactor: カラム名を修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_07_145451) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_09_151136) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,7 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_07_145451) do
     t.string "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "Completed", default: false
+    t.boolean "completed", default: false
     t.index ["post_id"], name: "index_tasks_on_post_id"
   end
 
